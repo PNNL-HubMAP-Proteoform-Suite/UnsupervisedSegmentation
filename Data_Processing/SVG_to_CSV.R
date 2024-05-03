242e2114045be6a8df4b0d2547a56c79f872e879
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2024-05-03 10:33:08 -0700

    updating with truth masks

--- Data_Processing/SVG_to_CSV.R
+++ Data_Processing/SVG_to_CSV.R
@@ -59,65 +59,72 @@ SVG_to_CSV <- function(image_path,
   
   ProcessedImage <- left_join(ProcessedImage, ColorConverter[, c("RGB", "Cluster")], by = "RGB") %>%
     mutate(Width = max(Width) - Width + 1) %>%
-    select(-c(R,G,B,RGB))
+    select(-c(R,G,B,RGB)) %>%
+    mutate(Cluster = as.factor(as.numeric(as.factor(Cluster)))) %>%
+    rename(Height = Width, Width = Length)
   
-  thePlot <- ggplot(ProcessedImage, aes(x = Length, y = Width, fill = Cluster)) +
-    geom_tile() +
-    theme_bw()
+  return(ProcessedImage)
   
-  
-  return(list(ProcessedImage, thePlot))
-  
+}
+
+make_plot <- function(ProcessedImage, ColorScale) {
+  ggplot(ProcessedImage, aes(x = Width, y = Height, fill = Cluster)) +
+    geom_raster(interpolate = TRUE) +
+    theme_void() +
+    scale_fill_manual(values = ColorScale) +
+    theme(legend.position = "none")
+}
+
+format_mask <- function(ProcessedImage) {
+  matrix(ProcessedImage$Cluster, nrow = max(ProcessedImage$Height), ncol = max(ProcessedImage$Width))
 }
 
 ## Make List of Features ## 
 Features <- data.frame(
-  symbol = c("Islet", "Islet", "Smear", "Smear", "Glomerulus", "Glomerulus",
-             "Tubule", "Tubule", "Blood.Vessel", "Blood.Vessel", "Empty.Space", "Empty.Space"),
-  red = c(0, 195, 234, 249, 183, 244, 43, 191, 70, 199, 192, 230),
-  green = c(126, 216, 51, 193, 171, 230, 41, 190, 8, 180, 190, 235),
-  blue = c(0, 188, 35, 189, 193, 245, 120, 214, 27, 186, 62, 197),
-  thresh = c(30, 30, 30, 30, 20, 20, 30, 30, 30, 30, 20, 20)
+  symbol = c("Green", "Green", "Pink", "Pink", "Purple", "Purple", "Red", "Red", "Yellow", "Yellow"),
+  red = c(55, 195, 220, 244, 70, 199, 234, 249, 208, 236),
+  green = c(126, 216, 171, 230, 8, 180, 51, 193, 207, 235),
+  blue = c(34, 188, 22, 245, 27, 186, 35, 189, 111, 197),
+  thresh = c(30, 30, 10, 10, 30, 30, 30, 30, 20, 20)
 )
 
 ## Pull SVGs ##
-SVGs <- list.files("/Users/degn400/Library/Group Containers/UBF8T346G9.OneDriveStandaloneSuite/OneDrive - PNNL.noindex/OneDrive - PNNL/Desktop/HubMAP/ImageIntegrator/Annotations_Final/Kidney",
-                  full.names = T)
-SVGs <- SVGs[grepl("svg", SVGs)]
 
-## Read Annotations ## 
-Annotations <- read.csv("~/Git_Repos/UnsupervisedSegmentation/Metadata/Kidney_Annotations_Summary.csv")
+# Tile 1------------------------------------------------------------------------
+Mask1 <- SVG_to_CSV("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_SVG/KPMP_uS-X001Y006_Annotations.svg", 
+                   Features %>% filter(symbol %in% c("Green", "Pink", "Purple", "Red")))
+Img1 <- make_plot(Mask1, c("white", "green", "pink", "purple", "red"))
+ggsave("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_PNG/KPMP_uS-X001Y006_Annotations.png", plot = Img1,
+       units = "px", height = max(Mask1$Height), width = max(Mask1$Width))
+write.table(format_mask(Mask1), "~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_TXT/KPMP_uS-X001Y006_Annotations.txt",
+            quote = F, row.names = F, sep = "\t")
 
-# Iterate through each image, and make sure there is the correct number of clusters 
-lapply(SVGs, function(SVG) {
-  
-  # Simplify the path
-  SimplePath <- SVG %>% strsplit("/") %>% unlist() %>% tail(1)
-  message(SimplePath)
-  
-  # Pull known features
-  Known <- Annotations[Annotations$Path == SimplePath, 2:7]
-  KnownFeatures <- colnames(Known)[Known != 0]
-  
-  # Run pipeline
-  Res <- SVG_to_CSV(SVG, Features %>% filter(symbol %in% KnownFeatures))
-  
-  # Make sure the number of clusters is correct
-  if (length(unique(Res[[1]]$Cluster)) == Annotations[Annotations$Path == SimplePath, "Total.Feature.Classes"]) {
-    NewPath <- gsub(".svg", ".csv", SimplePath, fixed = T)
-    NewHeader <- SVGs[1] %>% strsplit("/") %>% unlist() %>% head(-1) %>% paste0(collapse = "/")
-    NewPath <- file.path(NewHeader, NewPath)
-    CleanResults <- matrix(Res[[1]]$Cluster, nrow = max(Res[[1]]$Width), ncol = max(Res[[1]]$Length))
-    CleanResults[CleanResults == "Background"] <- NA
-    fwrite(CleanResults, NewPath)
-    ggsave(filename = gsub(".csv", "_extraction.png", NewPath), plot = Res[[2]])
-  } else {
-    browser()
-  }
-  
-})
+# Tile 2------------------------------------------------------------------------
+Mask2 <- SVG_to_CSV("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_SVG/KPMP_uS-X001Y007_Annotations.svg", 
+                    Features %>% filter(symbol %in% c("Green", "Purple")))
+Img2 <- make_plot(Mask2, c("white", "green", "purple"))
+ggsave("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_PNG/KPMP_uS-X001Y007_Annotations.png", plot = Img2,
+       units = "px", height = max(Mask2$Height), width = max(Mask2$Width))
+write.table(format_mask(Mask2), "~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_TXT/KPMP_uS-X001Y007_Annotations.txt",
+            quote = F, row.names = F, sep = "\t")
 
+# Tile 3------------------------------------------------------------------------
+Mask3 <- SVG_to_CSV("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_SVG/KPMP_uS-X001Y008_Annotations.svg", 
+                    Features %>% filter(symbol %in% c("Green", "Pink", "Red")))
+Img3 <- make_plot(Mask3, c("white", "green", "pink", "red"))
+ggsave("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_PNG/KPMP_uS-X001Y008_Annotations.png", plot = Img3,
+       units = "px", height = max(Mask3$Height), width = max(Mask3$Width))
+write.table(format_mask(Mask3), "~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_TXT/KPMP_uS-X001Y008_Annotations.txt",
+            quote = F, row.names = F, sep = "\t")
 
+# Tile 4------------------------------------------------------------------------
+Mask4 <- SVG_to_CSV("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_SVG/KPMP_uS-X001Y009_Annotations.svg", 
+                    Features %>% filter(symbol %in% c("Green", "Pink", "Red", "Yellow", "Purple")))
+Img4 <- make_plot(Mask4, c("white", "green", "pink", "purple" "red", "yellow"))
+ggsave("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_PNG/KPMP_uS-X001Y008_Annotations.png", plot = Img3,
+       units = "px", height = max(Mask3$Height), width = max(Mask3$Width))
+write.table(format_mask(Mask3), "~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/Manual_Segmentation_Masks_TXT/KPMP_uS-X001Y008_Annotations.txt",
+            quote = F, row.names = F, sep = "\t")
 
 
 
