7b01304354a375714fcac2141562d0fda6348c83
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2024-05-06 11:02:03 -0700

    Working through first segmentation masks

--- Visualization/render_clusters.R
+++ Visualization/render_clusters.R
@@ -3,7 +3,8 @@ library(tidyverse)
 
 render_cluster <- function(data, colors, order) {
   
-  colors <- colors[order]
+  colorList <- colors
+  names(colorList) <- order
   
   data %>%
     mutate(Height = 1:nrow(.)) %>%
@@ -13,7 +14,7 @@ render_cluster <- function(data, colors, order) {
            Cluster = as.factor(Cluster)) %>% 
     ggplot(aes(x = Width, y = Height, fill = Cluster)) +
     geom_raster(interpolate = TRUE) +
-    scale_fill_manual(values = colors) +
+    scale_fill_manual(values = colorList) +
     theme_void() +
     theme(legend.position = "none")
   
@@ -24,13 +25,14 @@ render_cluster <- function(data, colors, order) {
 # Clusters need to be matched manually 
 Image_Metadata <- fread("~/Git_Repos/UnsupervisedSegmentation/Metadata/Kidney_Annotations_Summary.csv")
 
-tile <- 8
+tile <- 25
 root <- unique(Image_Metadata$Path)[tile]
 data <- fread(file.path("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/KMeans_TXT", 
                         gsub(pattern = "Annotations", replacement = "KMeans.txt", root)))
 plot <- render_cluster(data,
                        unlist(Image_Metadata[Image_Metadata$Path == root, Color]),
                        unlist(Image_Metadata[Image_Metadata$Path == root, KMeans]))
+plot
 ggsave(file.path("~/Git_Repos/UnsupervisedSegmentation/Images/Kidney_Tiles/KMeans_PNG", 
                  gsub("_Annotations", "_KMeans.png", root)),  plot = plot,
        units = "px", height = nrow(data), width = ncol(data))
