025c779d6ded327c16b547f99e9f57d6052c3664
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2024-07-12 14:16:03 -0700

    Save results

--- Visualization/render_clusters.R
+++ Visualization/render_clusters.R
@@ -24,7 +24,7 @@ render_cluster <- function(data, colors, order) {
 
 # KMeans------------------------------------------------------------------------
 
-target <- 1
+target <- 2
 
 Image_Metadata <- fread("~/Git_Repos/UnsupervisedSegmentation/Metadata/Kidney_Annotations_Summary.csv") %>%
   filter(Blur == "X")
@@ -135,7 +135,7 @@ lapply(target, function(subtile) {
 
 
 
-# Superpixels-------------------------------------------------------------------
+# Supercells---------------------------------------------------------------------
 
 Image_Metadata <- fread("~/Git_Repos/UnsupervisedSegmentation/Metadata/Kidney_Annotations_Summary.csv") %>%
   filter(Blur == "X")
